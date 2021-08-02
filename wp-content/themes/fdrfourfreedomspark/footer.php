6b5b07201ad00caa33f2931215f7695d7636167f
dzikri <nadhimulloh@gmail.com>
2021-08-02 13:33:20 +0700

    wip general css

--- wp-content/themes/fdrfourfreedomspark/footer.php
+++ wp-content/themes/fdrfourfreedomspark/footer.php
@@ -1,5 +1,5 @@
     <?php get_template_part( 'parts/bottom' ); ?>
-    <?php get_template_part( 'parts/debugger' ); ?>
+    <?php //get_template_part( 'parts/debugger' ); ?>
 		<?php wp_footer(); ?>		
 	</body>
 </html>
