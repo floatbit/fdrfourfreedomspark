d850080da4d72cffa02781d95ae614c54c2a2ed9
tama <tama.wmp@gmail.com>
2021-08-03 13:27:07 +0700

    add new shortcode

--- wp-content/themes/fdrfourfreedomspark/shortcodes.php
+++ wp-content/themes/fdrfourfreedomspark/shortcodes.php
@@ -13,7 +13,7 @@
             return '';
         }
         
-        $args['target'] = ($args['target'] != null) ? $args['label'] : '';
+        $args['target'] = ($args['target'] != null) ? $args['target'] : '';
         $args['additional_class'] = ($args['additional_class'] != null) ? $args['additional_class'] : '';
 
         $html = '<a href="'.$args['url'].'" class="button '.$args['mode'].' '.$args['additional_class'].'" target="'.$args['target'].'">'.$args['label'].'</a>';
@@ -33,5 +33,26 @@
         return $html;
     }
     add_shortcode('link-with-icon', 'ffp_link_with_icon_shortcode');
+
+    function ffp_link_with_arrow_shortcode($args=array()) {
+        if (!$args) {
+            $args = array();
+        }
+
+        if ( empty( $args['url'] ) ) {
+            return '';
+        } 
+        
+        if ( empty( $args['label'] ) ) {
+            return '';
+        }
+        
+        $args['target'] = ($args['target'] != null) ? $args['target'] : '';
+        $args['class'] = ($args['class'] != null) ? $args['class'] : '';
+
+        $html = '<a href="'.$args['url'].'" class="button with-arrow '.$args['class'].'" target="'.$args['target'].'">'.$args['label'].'</a>';
+        return $html;
+    }
+    add_shortcode('button', 'ffp_link_with_arrow_shortcode');
 
 ?>
