cc52871ba6f5724519ed5b4b79555b135a852b96
tama <tama.wmp@gmail.com>
2021-08-04 08:03:33 +0700

    fix style

--- wp-content/themes/fdrfourfreedomspark/assets/src/js/page-home.js
+++ wp-content/themes/fdrfourfreedomspark/assets/src/js/page-home.js
@@ -1,3 +1,14 @@
 jQuery(document).ready(function($) {
-
+    $('[href="#home-upcoming-event"]').on('click', function(e){
+        e.preventDefault();
+        var url = $(this).attr("href");
+        if (url.includes('#')) {
+            e.preventDefault();
+            if ($(url).length !== 0) {
+                $([document.documentElement, document.body]).animate({
+                scrollTop: $(url).offset().top
+                }, 1000);
+            }
+        }
+    })
 })
