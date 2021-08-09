e179e690666bcb7dc9a8d315b492427c3a48758f
dzikri <nadhimulloh@gmail.com>
2021-08-09 09:07:00 +0700

    wip timeline

--- wp-content/themes/fdrfourfreedomspark/templates/page-learn-timeline.php
+++ wp-content/themes/fdrfourfreedomspark/templates/page-learn-timeline.php
@@ -9,12 +9,14 @@
 <main class="border-side">
 	
 	<div class="grid-x grid-padding-x pos-relative vb-1 vb-2 vb-3 border-top border-bottom">
-		<div class="cell medium-6 left-container flex-container align-center flex-dir-column">
-			<h1><?php the_title();?></h1>
-			<div class="content h3-style">
-				<?php the_content();?>
-			</div>
-			<?php print do_shortcode( '[link-with-arrow title="View Timeline" url="#expand-timeline"]' ); ?>	
+		<div class="cell medium-6 left-container flex-container align-center flex-dir-column hor-pad-right-expanded">
+			<div class="hor-pad-right-expanded">
+				<h1><?php the_title();?></h1>
+				<div class="content h3-style">
+					<?php the_content();?>
+				</div>
+				<?php print do_shortcode( '[link-with-arrow title="View Timeline" url="#expand-timeline"]' ); ?>
+			</div>	
 		</div>  
 		<div class="cell medium-6">
 
