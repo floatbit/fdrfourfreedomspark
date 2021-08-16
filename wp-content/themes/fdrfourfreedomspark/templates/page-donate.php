3d0ea9f599b6cf9595c8031eaa0b1b9cac827ec5
Juned <juned.tan@gmail.com>
2021-08-16 18:28:43 +0700

    wip review

--- wp-content/themes/fdrfourfreedomspark/templates/page-donate.php
+++ wp-content/themes/fdrfourfreedomspark/templates/page-donate.php
@@ -17,7 +17,7 @@
 		<?php foreach($content_rows as $item): ?>
 			<div class="grid-x grid-padding-x pos-relative vb-1 vb-3 border-top vert-pad-top-expanded vert-pad-bottom-expanded">
 				<?php if($item['title'] != null) : ?>
-					<div class="cell medium-3 vert-margin-top vert-margin-bottom">
+					<div class="cell medium-3 vert-margin-bottom">
 						<h1>
 							<?php print $item['title']; ?>
 						</h1>
@@ -26,17 +26,16 @@
 					<div class="cell medium-3"></div>
 				<?php endif; ?>
 				<?php if($item['content'] != null) : ?>
-					<div class="cell medium-6 middle-content vert-margin-top vert-margin-bottom">
+					<div class="cell medium-6 middle-content">
 						<?php print $item['content']; ?>
 					</div>
 				<?php else: ?>
 					<div class="cell medium-6"></div>
 				<?php endif; ?>
 				<?php if($item['right_text'] != null) : ?>
-					<div class="cell medium-3 vert-margin-top vert-margin-bottom">
+					<div class="cell medium-3">
 						<?php
-							$text = trim( str_replace('<p>', '', $item['right_text']));
-							$text = trim( str_replace('</p>', '', $text));
+							$text = $item['right_text'];
 						?>
 						<small><?php print $text; ?></small>
 					</div>
