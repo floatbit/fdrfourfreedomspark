5cc80d191b857fba68b37416e9dd249e13dd1111
Juned <juned.tan@gmail.com>
2021-08-19 09:52:21 +0700

    wip ig feed

--- wp-content/themes/fdrfourfreedomspark/parts/panel-instagram-feed.php
+++ wp-content/themes/fdrfourfreedomspark/parts/panel-instagram-feed.php
@@ -1,10 +1,10 @@
 <section id="ig-feed">
 	<div class="grid-x grid-padding-x pos-relative vb-1 vb-2 vb-3 content-inner-container vert-pad-top-expanded vert-pad-bottom-expanded">
-		<div class="cell medium-3">
+		<div class="cell medium-3 vert-margin-top">
 			<div class="p-style content-eyebrow color-black">Tag Your Photos</div>
 			<h2 class="h1-style content-title">#fourfreedomspark</h2>
 		</div>
-		<div class="cell medium-9">
+		<div class="cell medium-9 vert-margin-top">
 			<?php print do_shortcode('[instagram-feed]'); ?>
 		</div>
 	</div>
