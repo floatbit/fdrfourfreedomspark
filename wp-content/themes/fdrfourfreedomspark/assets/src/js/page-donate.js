a231238019d545fb74eac19c1f1db5d2a6c25a83
Juned <juned.tan@gmail.com>
2021-08-21 00:42:24 +0700

    style update

--- wp-content/themes/fdrfourfreedomspark/assets/src/js/page-donate.js
+++ wp-content/themes/fdrfourfreedomspark/assets/src/js/page-donate.js
@@ -2,6 +2,7 @@ jQuery(document).ready(function($) {
 	// default value for donation qty
 	$("#input_3_16_1").val('1');
 	// autofill payment amount on "other" donation change
+	$("#input_3_5_other").attr("type", "number");
 	$("#input_3_5_other").on("change", function() {
 		var value = $(this).val();
 		$("#input_3_16").html(value);
