bd6ab5bc550464a4b4b565c8308b7fc7569bc48e
Juned <juned.tan@gmail.com>
2021-11-18 17:44:59 +0700

    overflow body

--- wp-content/themes/fdrfourfreedomspark/functions.php
+++ wp-content/themes/fdrfourfreedomspark/functions.php
@@ -1,5 +1,5 @@
 <?php
-define( 'CSS_JS_VERSION', "1.17");
+define( 'CSS_JS_VERSION', "1.18");
 define( 'TEMPLATE_PATH', get_bloginfo('stylesheet_directory'));
 define( 'TEMPLATE_IMAGE_PATH', get_bloginfo('stylesheet_directory') . '/assets/img');
 
