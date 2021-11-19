349eb2cabb95910a06c2ebb06d3da77f5243c07a
Juned <juned.tan@gmail.com>
2021-11-19 17:52:07 +0700

    404 page

--- wp-content/themes/fdrfourfreedomspark/404.php
+++ wp-content/themes/fdrfourfreedomspark/404.php
@@ -1,18 +1,5 @@
-<?php get_header();?>
-
-<?php the_post();?>
-
-<main>
-  
-  <div class="grid-container">
-    <div class="grid-x grid-margin-x">
-      <div class="cell">
-        <h1>Page Not Found</h1>
-        <p>Oops. The page you're looking for doesn't want to be found.</p>
-      </div>
-    </div>
-  </div>                        
-
-</main>
-
-<?php get_footer(); ?>
+<?php
+header("HTTP/1.1 301 Moved Permanently");
+header("Location: ".get_bloginfo('url'));
+exit();
+?>
